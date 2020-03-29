41587824fd7e32c512e960bbc46a40f548257fcf
Sandeep <sandeeps4284@gmail.com>
2020-03-29 20:03:20 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About(){
+    return(
+        <React.Fragment>
+            <h1>About</h1>
+            <p>todo list app</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
